d9a7fb20b51a3a6fff5c9a905a63d59b8f22977e
J.T.Sage <jtsage+tdtrac@gmail.com>
2011-05-21 00:07:20 -0400

    Update demo page

--- index.php
+++ index.php
@@ -17,123 +17,148 @@
 		<h1>jQueryMobile - SimpleDialog Demo (1.0a4.1 Base)</h1>
 	</div>
 	<div data-role="content" data-theme="c">
-		<script type="text/javascript">
-			$('#diag1').live('click', function() {
-				$(this).simpledialog({
-					'mode' : 'bool',
-					'prompt' : 'Pick One!',
-					'buttons' : {
-						'Yes': {
-							click: function () { 
-								$('#diag1').find('.ui-btn-text').text(
-									$('#diag1').find('.ui-btn-text').text() + ' -- YES!'
-								);
-							}
-						},
-						'No': {
-							click: function () { 
-								$('#diag1').find('.ui-btn-text').text(
-									$('#diag1').find('.ui-btn-text').text() + ' -- NO!'
-								);
-							},
-							icon: "delete",
-							theme: "c"
-						}
-					}
-				})
-			});
-			
-			$('#diag2').live('click', function() {
-				$(this).simpledialog({
-					'mode' : 'string',
-					'prompt' : 'What is your name?',
-					'buttons' : {
-						'OK': {
-							click: function () { 
-								$('#diag2').find('.ui-btn-text').text(
-									$('#diag2').find('.ui-btn-text').text() + 
-									' -- ' + 
-									$('#diag2').attr('data-string')
-								);
-							}
-						},
-						'Cancel': {
-							click: function () { console.log(this); },
-							icon: "delete",
-							theme: "c"
-						}
-					}
-				})
-			});
-			
-			$('#diag3').live('click', function() {
-				$(this).simpledialog({
-					'mode' : 'bool',
-					'prompt': false,
-					'allowReopen': false,
-					'buttons' : {
-						'Yes': {
-							click: function () {
-								$('#diag3').find('.ui-btn-text').text(
-									$('#diag3').find('.ui-btn-text').text() + ' -- YES!'
-								);
-							}
-						},
-						'No': {
-							click: function () {
-								$('#diag3').find('.ui-btn-text').text(
-									$('#diag3').find('.ui-btn-text').text()+' -- NO!'
-								); },
-							icon: "delete"
-						}
-					}
-				})
-			});
-			
-			//This is how the form works:
-			
-			$('select').live('change', function(e) { // BEGIN : Add Dropdown Option
-				var self = this;
-		
-				$(self+':selected:not([data-placeholder])').each(function(){
-					if ( $(this).attr('data-addoption') ) {
-						$(self).simpledialog({
-							'mode' : 'string',
-							'prompt' : 'Add New Option',
-							'useDialogForceFalse' : true,
+		<div data-role="collapsible">
+			<h3>Simple Demos</h3>
+			<div data-role="collapsible">
+				<h3>Boolean Dialog (src line: 23)</h3>
+				<script type="text/javascript">
+					$('#diag1').live('click', function() {
+						$(this).simpledialog({
+							'mode' : 'bool',
+							'prompt' : 'Pick One!',
 							'buttons' : {
-								'Yes, Add' : {
+								'Yes': {
 									click: function () { 
-										thisopt = $(self).attr('data-string');
-										$('<option value="'+thisopt+'" selected="selected">'+thisopt+'</option>').appendTo($(self));
-										$(self).selectmenu('refresh', true);
-										return true; }
+										$('#diag1').find('.ui-btn-text').text(
+											$('#diag1').find('.ui-btn-text').text() + ' -- YES!'
+										);
+									}
 								},
-								'Cancel' : {
-									click: function () { $(self).selectmenu('open'); },
+								'No': {
+									click: function () { 
+										$('#diag1').find('.ui-btn-text').text(
+											$('#diag1').find('.ui-btn-text').text() + ' -- NO!'
+										);
+									},
+									icon: "delete",
+									theme: "c"
+								}
+							}
+						})
+					});
+				</script>
+				<a href='#' id='diag1' data-role='button'>Choose Something</a>
+			</div>
+			<div data-role="collapsible">
+				<h3>String Dialog (src line: 53)</h3>
+				<script type="text/javascript">
+					$('#diag2').live('click', function() {
+						$(this).simpledialog({
+							'mode' : 'string',
+							'prompt' : 'What is your name?',
+							'buttons' : {
+								'OK': {
+									click: function () { 
+										$('#diag2').find('.ui-btn-text').text(
+											$('#diag2').find('.ui-btn-text').text() + 
+											' -- ' + 
+											$('#diag2').attr('data-string')
+										);
+									}
+								},
+								'Cancel': {
+									click: function () { console.log(this); },
+									icon: "delete",
+									theme: "c"
+								}
+							}
+						})
+					});
+				</script>
+				<a href='#' id='diag2' data-role='button'>Type Something</a>
+			</div>
+		</div>
+		<div data-role="collapsible" data-collapsed="true">
+			<h3>Advanced Demos</h3>
+			<div data-role="collapsible" data-collapsed="true">
+				<h3>One-Shot Boolean Dialog (src line: 83)</h3>
+				<p>With no prompt text</p>
+				<script type="text/javascript">
+					$('#diag3').live('click', function() {
+						$(this).simpledialog({
+							'mode' : 'bool',
+							'prompt': false,
+							'allowReopen': false,
+							'buttons' : {
+								'Yes': {
+									click: function () {
+										$('#diag3').find('.ui-btn-text').text(
+											$('#diag3').find('.ui-btn-text').text() + ' -- YES!'
+										);
+									}
+								},
+								'No': {
+									click: function () {
+										$('#diag3').find('.ui-btn-text').text(
+											$('#diag3').find('.ui-btn-text').text()+' -- NO!'
+										); },
 									icon: "delete"
 								}
 							}
-						});
-					}
-				});
-			}); // END : Add Dropdown Option
-			
-		</script>
-		<a href='#' id='diag1' data-role='button'>Boolean Dialog</a>	
-		<a href='#' id='diag2' data-role='button'>String Dialog</a>	
-		<a href='#' id='diag3' data-role='button'>One-Shot Boolean Dialog, No Prompt Text</a>
-		<br />
-		Why I wrote the string method...
-		<form method='get' action='post'>
-		<div data-role='fieldcontain'><label for='vendor'>Some Select Menu, using jQM styles</label><select data-native-menu="false" name='vendor' id='vendor' ><option data-placeholder='true'>Choose one...</option><option data-addoption='true' value='0'>Add New...</option><option value='Example Vendor'>Example Vendor</option><option value='Another Example Vendor'>Another Example Vendor</option></select></div> 
-		</form>
-		<div>
-		<?php
-		include_once "../markdown.php";
-		$md_text = file_get_contents("README.md");
-		echo Markdown($md_text);
-		?>
+						})
+					});
+				</script>
+				<a href='#' id='diag3' data-role='button'>Choose Something, But Just Once</a>
+			</div>
+			<div data-role="collapsible" data-collapsed="true">
+				<h3>Adding to a select list (src line: 114)</h3>
+				<p>a.k.a, Why I wrote the string method...</p>
+					<script type="text/javascript">
+						$('select').live('change', function(e) { // BEGIN : Add Dropdown Option
+							var self = this;
+							$(self+':selected:not([data-placeholder])').each(function(){
+								if ( $(this).attr('data-addoption') ) {
+									$(self).simpledialog({
+										'mode' : 'string',
+										'prompt' : 'Add New Option',
+										'useDialogForceFalse' : true,
+										'buttons' : {
+											'Yes, Add' : {
+												click: function () { 
+													thisopt = $(self).attr('data-string');
+													$('<option value="'+thisopt+'" selected="selected">'+thisopt+'</option>').appendTo($(self));
+													$(self).selectmenu('refresh', true);
+													return true; }
+											},
+											'Cancel' : {
+												click: function () { $(self).selectmenu('open'); },
+												icon: "delete"
+											}
+										}
+									});
+								}
+							});
+						}); // END : Add Dropdown Option
+					</script>
+					<div data-role='fieldcontain'>
+						<label for='vendor'>Some Select Menu, using jQM styles</label>
+						<select data-native-menu="false" name='vendor' id='vendor' >
+							<option data-placeholder='true'>Choose one...</option>
+							<option data-addoption='true' value='0'>Add New...</option>
+							<option value='Example Vendor'>Example Vendor</option>
+							<option value='Another Example Vendor'>Another Example Vendor</option>
+						</select>
+					</div> 
+			</div>
+		</div>
+		<div data-role="collapsible">
+			<!-- NOTE: When running locally, just nuke this section, or go grab PHP Markdown -->
+			<h3>Readme File</h3>
+			<?php
+				include_once "../markdown.php";
+				$md_text = file_get_contents("README.md");
+				echo Markdown($md_text);
+			?>
 		</div>
 	</div>
 	<div data-role="footer">
