0da754f3496ec8188828e943fb4c0952bef958d6
J.T. Sage <jtsage+github@gmail.com>
2012-06-11 17:23:16 -0400

    Update build system

--- build/make-all.sh
+++ build/make-all.sh
@@ -1,1 +1,4 @@
-./make.pl all && ./make.pl all 1.0.1 && rm *.bac && mv jquery.mobile.simpledialog*1.0.1* ../../cdn/simpledialog/1.0.1/ && ./make.pl all 1.0.0 && rm *.bac && mv jquery.mobile.simpledialog*1.0.0* ../../cdn/simpledialog/1.0.0/
+./make.pl all 
+./make.pl all 1.0.0 && rm *.bac && mv jquery.mobile.simpledialog*1.0.0* ../../cdn/simpledialog/1.0.0/
+./make.pl all 1.0.1 && rm *.bac && mv jquery.mobile.simpledialog*1.0.1* ../../cdn/simpledialog/1.0.1/ 
+./make.pl all 1.1.0 && rm *.bac && mv jquery.mobile.simpledialog*1.1.0* ../../cdn/simpledialog/1.0.0/
